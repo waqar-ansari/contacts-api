caabf7d7dec06c0531e9606baf1b7fbcbb9c5202
Waqar Ansari <waqar.78692@yahoo.com>
2025-03-19 19:23:15 +0400

    changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-const v1Router = express.Router();
+
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require("./swaggerConfig");
 const userRoutes = require("./routes/userRoutes");
@@ -33,20 +33,20 @@ app.use(express.json());
 app.use(express.static(path.resolve("./public")));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-v1Router.use("/edit-profile_api", checkForAuthentication(), editProfileRoutes);
-v1Router.use("/user", userRoutes);
-v1Router.use("/delete-contact_api", deleteContactRoutes);
-v1Router.use("/delete-user_api",checkForAuthentication(), deleteUserRoutes);
-v1Router.use("/add-tag_api", checkForAuthentication(), addTagRoutes);
-v1Router.use("/get-tag_api", checkForAuthentication(), getTagRoutes);
-v1Router.use("/delete-tag_api", checkForAuthentication(), deleteTagRoutes);
-v1Router.use("/addtofavourite_api", checkForAuthentication(), addToFavouriteRoutes);
-v1Router.use("/get-contact_api", checkForAuthentication(), getContactRoutes);
-v1Router.use("/add-edit-contact_api", checkForAuthentication(), contactRoutes);
-v1Router.use("/whoScannedMe", checkForAuthentication(), whoScannedMeRoutes);
-v1Router.use("/iScannedWho", checkForAuthentication(), iScannedWhoRoutes);
+app.use("/edit-profile_api", checkForAuthentication(), editProfileRoutes);
+app.use("/user", userRoutes);
+app.use("/delete-contact_api", deleteContactRoutes);
+app.use("/delete-user_api",checkForAuthentication(), deleteUserRoutes);
+app.use("/add-tag_api", checkForAuthentication(), addTagRoutes);
+app.use("/get-tag_api", checkForAuthentication(), getTagRoutes);
+app.use("/delete-tag_api", checkForAuthentication(), deleteTagRoutes);
+app.use("/addtofavourite_api", checkForAuthentication(), addToFavouriteRoutes);
+app.use("/get-contact_api", checkForAuthentication(), getContactRoutes);
+app.use("/add-edit-contact_api", checkForAuthentication(), contactRoutes);
+app.use("/whoScannedMe", checkForAuthentication(), whoScannedMeRoutes);
+app.use("/iScannedWho", checkForAuthentication(), iScannedWhoRoutes);
 
-app.use("/api/v1", v1Router);
+// app.use("/api/v1", v1Router);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
