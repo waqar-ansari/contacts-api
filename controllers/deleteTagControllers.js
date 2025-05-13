5766ff55c8fa0ecab11bfbb381925c7ef83b4bad
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-05-13 18:09:06 +0530

    some chnages in delete tag api

--- controllers/deleteTagControllers.js
+++ controllers/deleteTagControllers.js
@@ -1,5 +1,7 @@
 const Contact = require("../models/contactModel");
 const User = require("../models/userModel");
+const mongoose = require("mongoose");
+
 
 const deleteTag = async (req, res) => {
   try {
@@ -8,20 +10,34 @@ const deleteTag = async (req, res) => {
     if (!user) {
       return res
         .status(404)
-        .json({ status: "error", message: "User not found"});
+        .json({ status: "error", message: "User not found" });
     }
 
     if (!tag_id) {
       return res
         .status(404)
-        .json({  status: "error",message: "Tag not found", });
+        .json({ status: "error", message: "Tag not found", });
     }
     const tag = user.tags.find((tag) => tag.tag_id.toString() === tag_id);
     if (!tag) {
       return res
         .status(404)
-        .json({  status: "error",message: "Tag not found" });
+        .json({ status: "error", message: "Tag not found" });
     }
+
+    // // Find the tag object in user.tags by tag_id
+    // const tagObject = user.tags.find(
+    //   (tag) => tag.tag_id.toString() === tag_id
+    // );
+
+    // if (!tagObject) {
+    //   return res.status(404).json({
+    //     status: "error",
+    //     message: "Tag not found in user",
+    //   });
+    // }
+
+    // const tagName = tagObject.tag; // This is the string stored in contacts
 
     user.tags.pull({ tag_id });
     await Contact.updateMany(
@@ -29,11 +45,19 @@ const deleteTag = async (req, res) => {
       { $pull: { tags: { tag_id } } }
     );
     await user.save();
+
+    // const contactUpdateResult = await Contact.updateMany(
+    //   { tags: tagName },
+    //   { $pull: { tags: tagName } }
+    // );
+
     res
       .status(200)
-      .json({  status: "success" ,message: "Tag deleted successfully", tagId : tag_id });
+      .json({
+        status: "success", message: "Tag deleted successfully", data: { tag_id }
+      });
   } catch {
-    res.status(500).send({  status: "error",message: "Error deleting tag"});
+    res.status(500).send({ status: "error", message: "Error deleting tag" });
   }
 };
 
