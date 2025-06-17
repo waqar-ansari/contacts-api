2b7960db874abae6f4b6540a1966323b30c5f7c4
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-06-17 18:03:34 +0530

    some changes

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const { checkForAuthentication } = require("./middlewares/authentication");
 const scanRoutes = require("./routes/scanRoutes");
 const reminderRoutes = require("./routes/reminderRoutes");
 const { error } = require("console");
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
 app.use(cors());
 
@@ -98,7 +98,7 @@ app.use("/", (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to Database");
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    // app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (err) {
     console.error("Database connection failed:", err);
   }
