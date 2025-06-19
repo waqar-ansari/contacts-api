1da57d2edd7e4ceb31d584eb9b336fb69b304110
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-06-19 14:10:43 +0530

    remove extra space in default img

--- models/userModel.js
+++ models/userModel.js
@@ -208,7 +208,7 @@ const userSchema = new Schema(
 
     profileImageURL: {
       type: String,
-      default: "https://contacts-api-bucket.s3.eu-north-1.amazonaws.com/profileImages/defaultImage.jpeg ",
+      default: "https://contacts-api-bucket.s3.eu-north-1.amazonaws.com/profileImages/defaultImage.jpeg",
     },
     resetPasswordToken: { type: String },
     resetPasswordExpires: { type: Date },
