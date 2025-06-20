cb4d26084701d8938144b6d653c9325f82bfac43
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-06-20 17:44:22 +0530

    change in index file

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const reminderRoutes = require("./routes/reminderRoutes");
 const userInfoRoutes = require("./routes/userInfoRoutes");
 const getUserCardRoutes = require("./routes/getUserCardRoutes");
 const { error } = require("console");
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
 app.use(cors());
 
@@ -108,7 +108,7 @@ app.use("/", (req, res) => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Connected to Database");
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    // app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
   } catch (err) {
     console.error("Database connection failed:", err);
   }
