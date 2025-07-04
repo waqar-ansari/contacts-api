c877211266e1bcd74788d0a1c74c806d8b7e4638
Waqar Ansari <waqar.78692@yahoo.com>
2025-07-04 12:10:12 +0400

    bulk contacts

--- controllers/saveBulkContactsController.js
+++ controllers/saveBulkContactsController.js
@@ -3,9 +3,6 @@ const User = require("../models/userModel");
 const mongoose = require("mongoose");
 
 const saveBulkContacts = async (req, res) => {
-  console.log(req.body, "called");
-  console.log(req.user._id, "userid");
-  console.log(req.user, "request user");
   try {
     const user = await User.findById(req.user._id);
     if (!user) {
@@ -14,11 +11,8 @@ const saveBulkContacts = async (req, res) => {
         message: "Unauthorized: User not found",
       });
     }
-    console.log(req.body, "req body");
 
     const { contacts } = req.body;
-    console.log(contacts, "contactssss");
-
     if (!Array.isArray(contacts) || contacts.length === 0) {
       return res.status(400).json({
         status: "error",
@@ -26,7 +20,10 @@ const saveBulkContacts = async (req, res) => {
       });
     }
 
-    const bulkPayload = contacts.map((contact) => {
+    const bulkPayload = [];
+    const skippedContacts = [];
+
+    for (const contact of contacts) {
       const {
         firstname,
         lastname,
@@ -44,8 +41,40 @@ const saveBulkContacts = async (req, res) => {
       const parsedPhones = Array.isArray(phonenumbers)
         ? phonenumbers.map((num) => String(num).replace(/[^\d]/g, ""))
         : [String(phonenumbers).replace(/[^\d]/g, "")];
+
+      const emailList = Array.isArray(emailaddresses)
+        ? emailaddresses
+        : emailaddresses
+        ? [emailaddresses]
+        : [];
+      const phoneList = parsedPhones;
+
+      let isDuplicate = false;
+      if (emailList.length || phoneList.length) {
+        const duplicateQuery = {
+          createdBy: req.user._id,
+          $or: [],
+        };
+        if (emailList.length) {
+          duplicateQuery.$or.push({ emailaddresses: { $in: emailList } });
+        }
+        if (phoneList.length) {
+          duplicateQuery.$or.push({ phonenumbers: { $in: phoneList } });
+        }
+        if (duplicateQuery.$or.length > 0) {
+          const existing = await Contact.findOne(duplicateQuery);
+          if (existing) isDuplicate = true;
+        }
+      }
+
+      if (isDuplicate) {
+        skippedContacts.push(contact);
+        continue;
+      }
+
       const generatedId = new mongoose.Types.ObjectId();
-      return {
+      bulkPayload.push({
+        _id: generatedId,
         contact_id: generatedId,
         firstname,
         lastname,
@@ -59,16 +88,19 @@ const saveBulkContacts = async (req, res) => {
         emailaddresses,
         phonenumbers: parsedPhones,
         createdBy: req.user._id,
-      };
-    });
+      });
+    }
 
     const savedContacts = await Contact.insertMany(bulkPayload);
-    console.log(savedContacts, "bulk saved contacts");
 
     return res.status(201).json({
       status: "success",
-      message: "Contacts saved successfully",
-      data: savedContacts,
+      message: "Contacts added successfully",
+      data: savedContacts.map((c) => {
+        const { _id, __v, ...contact } = c.toObject();
+        return { ...contact };
+      }),
+      skipped: skippedContacts,
     });
   } catch (error) {
     console.error("Bulk contact save error:", error);
