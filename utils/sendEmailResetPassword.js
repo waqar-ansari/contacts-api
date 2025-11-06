7bb0125d4ba8c45bda8a8699f1fa150cd5df95e2
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-11-06 13:20:19 +0530

    currently change the email smtp

--- utils/sendEmailResetPassword.js
+++ utils/sendEmailResetPassword.js
@@ -1,20 +1,36 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
+  // const transporter = nodemailer.createTransport({
+  //   service: 'smtp',
+  //   host: "smtp.titan.email", // SMTP server address
+  //   port: 465, // Port for secure connection
+  //   secure: true, // Use SSL/TLS
+  //   auth: {
+  //     user: 'noreply@contacts.management', // your Gmail address
+  //     pass: 'bZ}JTus_PQ{qWvA'     // app password from Google
+  //     // user: "makvanayash12@gmail.com",
+  //     // pass: "fybb lnri tmrq otmg",
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false  // THIS LINE FIXES THE ERROR
+  //   }
+  // });
+
   const transporter = nodemailer.createTransport({
-    service: 'smtp',
-    host: "smtp.titan.email", // SMTP server address
-    port: 465, // Port for secure connection
+    service: "smtp", // Use your SMTP service
+    host: "smtp.gmail.com", // SMTP server address
+    port: 587, // Port for secure connection
     secure: true, // Use SSL/TLS
     auth: {
-      user: 'noreply@contacts.management', // your Gmail address
-      pass: 'bZ}JTus_PQ{qWvA'     // app password from Google
-      // user: "makvanayash12@gmail.com",
-      // pass: "fybb lnri tmrq otmg",
+      user: "daaimalisheikh23@gmail.com",
+      pass: "dstv vfwc uqjy qjic", // App Password, not normal password
     },
+    // optional TLS options:
     tls: {
-      rejectUnauthorized: false  // THIS LINE FIXES THE ERROR
-    }
+      // do not fail on invalid certs in dev (remove in prod)
+      rejectUnauthorized: false,
+    },
   });
 
   //  from: '"Contacts Management" <noreply@contacts.management>',
