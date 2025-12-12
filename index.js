42cc96c107033e2a8a36ba807742018f541f6dd5
daaim <daaimalisheikh23@gmail.com>
2025-12-12 15:35:51 +0500

    remove webhook route

--- index.js
+++ index.js
@@ -107,7 +107,7 @@ app.use(cors());
 
 // Webhook routes must be defined before JSON parsing middleware
 // because Stripe needs raw body for signature verification
-app.use("/webhooks", webhookRoutes);
+// app.use("/webhooks", webhookRoutes);
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
