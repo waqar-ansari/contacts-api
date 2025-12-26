5b7050e941749980a6cb15e5e82d4c9df1e9290a
makvanayash2112 <112868826+makvanayash2112@users.noreply.github.com>
2025-12-26 13:56:56 +0530

    change the email credentials

--- contacts-daily-cron/utils/sendEmail.js
+++ contacts-daily-cron/utils/sendEmail.js
@@ -7,16 +7,32 @@ const nodemailer = require("nodemailer");
  * @param {string} html - HTML content of the email
  */
 const sendEmail = async (to, subject, html) => {
+  // const transporter = nodemailer.createTransport({
+  //   service: "smtp",
+  //   host: "smtp.titan.email",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: "noreply@contacts.management",
+  //     pass: "bZ}JTus_PQ{qWvA",
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // });
+
   const transporter = nodemailer.createTransport({
-    service: "smtp",
-    host: "smtp.titan.email",
-    port: 465,
-    secure: true,
+    service: "smtp", // Use your SMTP service
+    host: "email-smtp.eu-north-1.amazonaws.com", // SMTP server address
+    port: 587, // Port for secure connection
+    secure: false, // Use SSL/TLS
     auth: {
-      user: "noreply@contacts.management",
-      pass: "bZ}JTus_PQ{qWvA",
+      user: "AKIAZPPGACVJJ6IDCLWH",
+      pass: "BI2f1DBxTOVGOaW04FdAEzG+aE6QBDYNF3h4+Xl65uYF", // App Password, not normal password
     },
+    // optional TLS options:
     tls: {
+      // do not fail on invalid certs in dev (remove in prod)
       rejectUnauthorized: false,
     },
   });
